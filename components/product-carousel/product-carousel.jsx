94b1492ae170919afc33fbfaed2482ac74c9d70a
jessoy <81160034+jessoy@users.noreply.github.com>
2022-05-10 14:14:27 +0100

    carousel third party added

--- components/product-carousel/product-carousel.jsx
+++ components/product-carousel/product-carousel.jsx
@@ -1,9 +1,46 @@
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import styles from "./product-carousel.module.scss";
+import Image from "next/image";
 
-const ProductCarousel = ({ image }) => {
+// imported third party carousel package
+
+const ProductCarousel = ({ imageUrls }) => {
+  // console.log(imageUrls);
+
+  imageUrls.map((url, index) => {
+    return (
+      <div key={index}>
+        <img src={url} alt="image1" />
+      </div>
+    );
+  });
+
   return (
     <div className={styles.productCarousel} aria-pretend="true">
-      <img src={image} alt="product image" style={{ width: "100%", maxWidth: "500px" }} />
+      {/* <img src={image} alt="product image" style={{ width: "100%", maxWidth: "500px" }} /> */}
+
+      <Carousel
+        showThumbs="false"
+        infiniteLoop="true"
+        // style={{ width: "100%", maxWidth: "500px" }}
+      >
+        {/* {imageUrls ? ( */}
+        {imageUrls.map((url, index) => {
+          return (
+            <div key={index}>
+              <img
+                src={url}
+                alt="product image"
+                style={{ width: "100%", maxWidth: "500px" }}
+              />
+              {/* <p className="legend">Image 1</p> */}
+            </div>
+          );
+        })}
+
+      </Carousel>
     </div>
   );
 };
