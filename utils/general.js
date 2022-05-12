bb6c9587182b0ba97d4ed26cfa0f5f02a0172f70
jessoy <81160034+jessoy@users.noreply.github.com>
2022-05-12 12:24:41 +0100

    text formatting

--- utils/general.js
+++ utils/general.js
@@ -1,29 +1,36 @@
-//returns an array of a string split into two parts at the first paragraph
-export const truncateDescription = (description) => {
-    const index = description.indexOf('<\/p>') +5;
-    return [description.slice(0, index), description.slice(index)]
+//returns an array of an original string split into two parts at the first paragraph
+export const truncateDescription = (string) => {
+  // for text formatted from start with <p> tags
+  if (string.indexOf("<") === 0) {
+    const index = string.indexOf("</p>") + 5;
+    return [string.slice(0, index), string.slice(index)];
+  } else {
+    // for text not begining with <p> tags
+    // improve with inserting spaces after full stops where required
+    const index = string.indexOf("<p>");
+    return [string.slice(0, index), string.slice(index)];
   }
-
-  export const sortAttributesAlphabetically = (x, y) => {
-    if (x.name < y.name) return -1
-    if (y.name < x.name) return 1
-    return 0
-  }
-
-export const restrictAPIResponse = (data, number) => {
-  if (data.length <= number) return data;
-  let newArray = [];
-    for (let i = 0; i < number; i++) {
-      newArray.push(data[i]);
-    }
-    return newArray
 };
 
-export const replaceNull = (description, replacement) => {
-  const index = description.indexOf('\"null\"');
-  return `${replacement} ${description.slice(index+7)}`
-}
+// sorts an array alphabetically by name
+export const sortAttributesAlphabetically = (x, y) => {
+  if (x.name < y.name) return -1;
+  if (y.name < x.name) return 1;
+  return 0;
+};
 
-// const description = "\"null\" results | John Lewis & Partners"
+// reduces Array to certain number
+export const restrictAPIResponse = (array, number) => {
+  if (array.length <= number) return array;
+  let newArray = [];
+  for (let i = 0; i < number; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
 
-// console.log(replaceNull(description))
+// replaces null text within string with replacement string
+export const replaceNull = (string, replacementString) => {
+  const index = string.indexOf('"null"');
+  return `${replacementString} ${string.slice(index + 7)}`;
+};
